e633d23f600321d24f3cc096fd4594b87c2337fa
Mohamed Ahmed <knightwing862@gmail.com>
2023-12-28 03:31:11 +0200

    Final Changes

--- app/api/auth/[...nextauth]/auth-config.ts
+++ app/api/auth/[...nextauth]/auth-config.ts
@@ -13,11 +13,11 @@ const authOptions: AuthOptions = {
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string
+      clientSecret: process.env.GITHUB_SECRET as string,
     }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
     CredentialsProvider({
       name: 'credentials',
@@ -25,31 +25,30 @@ const authOptions: AuthOptions = {
         email: { label: 'email', type: 'text' },
         password: { label: 'password', type: 'password' }
       },
-      async authorize(credentials) {
-        if (!credentials?.email || !credentials?.password) {
-          throw new Error('Invalid credentials');
+      async authorize(credentials) {if (!credentials?.email || !credentials?.password) {
+        throw new Error('Invalid credentials');
+      }
+
+      const user = await prisma.user.findUnique({
+        where: {
+          email: credentials.email
         }
+      });
 
-        const user = await prisma.user.findUnique({
-          where: {
-            email: credentials.email
-          }
-        });
+      if (!user || !user?.hashedPassword) {
+        throw new Error('Invalid credentials');
+      }
 
-        if (!user || !user?.hashedPassword) {
-          throw new Error('Invalid credentials');
-        }
+      const isCorrectPassword = await bcrypt.compare(
+        credentials.password,
+        user.hashedPassword
+      );
 
-        const isCorrectPassword = await bcrypt.compare(
-          credentials.password,
-          user.hashedPassword
-        );
+      if (!isCorrectPassword) {
+        throw new Error('Invalid credentials');
+      }
 
-        if (!isCorrectPassword) {
-          throw new Error('Invalid credentials');
-        }
-
-        return user;
+      return user;
       }
     })
   ],
